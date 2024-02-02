a1d7e1b042c5b22c1629f9b01701c1d3c6094461
Hassnain Zia <147612259+Hassnainzia@users.noreply.github.com>
2024-02-03 04:43:45 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  varient: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+};
